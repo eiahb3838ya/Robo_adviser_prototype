cdfa96aa71895f9fadb3ae632671ec4af7b92310
Evan <eiahb3838ya@gmail.com>
2019-07-16 11:16:03 +0800

    single target result use ajax for all the bbandma is done

--- robo_adviser/adviser/urls.py
+++ robo_adviser/adviser/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
+
 app_name = 'adviser'
 urlpatterns = [
     path('', views.start, name='start'),
-    path('strategy_bbandma/', views.strategy_bbandma, name='strategy_bbandma'),
+    path('bbandma_result/', views.bbandma_result, name='bbandma_result'),
     path('smawma_result/', views.smawma_result, name='smawma_result'),
     path('r_strategy_result/', views.r_strategy_result,name='r_strategy_result'),
 
     path('debuger_result1/', views.debuger_result1,name='debuger_result1'),
 
 
-    path('api/from_r/chart/return',views.SrategyFromRReturnData.as_view(),name="api_chart_return_data"),
-    path('api/smawma/table/all',views.SrategySMAWMATableData.as_view(),name="api_smawma_table_data")
+    path('api/from_r/chart/return',views.StrategyFromRReturnData.as_view(),name="api_chart_return_data"),
+    path('api/smawma/table/all',views.StrategySMAWMATableData.as_view(),name="api_smawma_table_data"),
+    path('api/bbandma/table/all',views.StrategyBBandMATableData.as_view(),name="api_bbandma_table_data"),
+
 ]
--- robo_adviser/adviser/views.py
+++ robo_adviser/adviser/views.py
@@ -8,36 +8,6 @@ import json
 def start(request):
     return(render(request,'enter_adviser.html',locals()))
 
-def strategy_bbandma(request):
-    try:
-        user_picked = request.GET['stockpicker']
-    except:
-        print("rrrrrr")
-    # get the history price with yahoo finance
-    target_price_df = data_generator.get_history_data(user_picked)
-    # call BBand
-    df, count, acmroi, winnum, winfact = BBandMA.main(target_price_df)
-
-    # some adjust for to_json properly
-    df = df.reset_index()
-    df_to_display = df.drop(axis=0, columns=['Open', 'High', 'Low','BBupper', 'BBlower'])
-    dates = df_to_display.Date
-    df_to_display['Date'] = dates.apply(lambda x: x.strftime('%Y-%m-%d'))
-
-    # prepare data for js
-    selected_target = user_picked
-    selected_strategy = "BBand"
-    result_table_dict = df_to_display.to_dict(orient='records', double_precision=4)
-    columns = [{'field': f, 'title': f} for f in df_to_display.columns]
-
-    context = {
-        "selected_target": selected_target,
-        "selected_strategy": "bbandma",
-        "data": result_table_dict,
-        "columns": columns,
-    }
-    return (render(request, "single_target_result.html", context))
-
 def smawma_result(request):
     try:
         selected_target = request.GET['stockpicker']
@@ -46,6 +16,17 @@ def smawma_result(request):
     context = {
         "selected_target": selected_target,
         "selected_strategy": "smawma",
+    }
+    return(render(request,"single_target_result_using_ajax.html", context))
+
+def bbandma_result(request):
+    try:
+        selected_target = request.GET['stockpicker']
+    except:
+        print("request.GET['stockpicker'] failed")
+    context = {
+        "selected_target": selected_target,
+        "selected_strategy": "bbandma",
     }
     return(render(request,"single_target_result_using_ajax.html", context))
 
@@ -61,7 +42,7 @@ def debuger_result1(request):
     return (render(request, 'result1.html', locals()))
 
 
-class SrategyFromRReturnData(APIView):
+class StrategyFromRReturnData(APIView):
     def get(self, request, format = None):
         selected_target = request.GET["selected_target"]
         print(selected_target)
@@ -71,7 +52,7 @@ class SrategyFromRReturnData(APIView):
         print("data_dict", data_dict)
         return( Response( data_dict ) )
 
-class SrategySMAWMATableData(APIView):
+class StrategySMAWMATableData(APIView):
     def get(self, request, format = None):
         selected_target = request.GET["selected_target"]
         print(selected_target)
@@ -105,5 +86,38 @@ class SrategySMAWMATableData(APIView):
             "result_table_dict": result_table_dict,
             "result_table_json":result_table_json,
             "columns": columns,
+        }
+        return( Response( data ) )
+class StrategyBBandMATableData(APIView):
+    def get(self, request, format = None):
+        selected_target = request.GET["selected_target"]
+        # print(selected_target)
+
+        # get the history price with yahoo finance
+        target_price_df = data_generator.get_history_data(selected_target)
+        # call BBand
+        df, count, acmroi, winnum, winfact = BBandMA.main(target_price_df)
+
+        # some adjust for to_json properly
+        df = df.reset_index()
+        df_to_display = df.drop(axis=0, columns=['Open', 'High', 'Low', 'BBupper', 'BBlower'])
+        dates = df_to_display.Date
+        df_to_display['Date'] = dates.apply(lambda x: x.strftime('%Y-%m-%d'))
+
+        df_to_display = df_to_display.fillna('')
+
+        # prepare data for js
+        selected_target = selected_target
+        result_table_dict = df_to_display.to_dict(orient='records')
+        result_table_json = df_to_display.to_json(orient='records')
+        columns = [{'field': f, 'title': f} for f in df_to_display.columns]
+        columns_json = json.dumps(columns)
+        data = {
+            "selected_target": selected_target,
+            "selected_strategy": "bbandma",
+            "result_table_dict": result_table_dict,
+            "result_table_json": result_table_json,
+            "columns": columns,
+            "columns_json": columns_json
         }
         return( Response( data ) )
